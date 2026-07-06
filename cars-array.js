67aa9752638ee7842569005a8deee85696cf05c5
Chioma Nnamani <nnamanivirginia5@gmail.com>
2026-07-06 17:31:57 +0100

    Cars Array practice completed

---
+++ cars-array.js
@@ -0,0 +1,14 @@
+// array
+let cars = ["toyota", "honda", "mazda", "mitsubishi", "tesla", "lexus"];
+
+console.log(cars[0]);
+let numOfCars = cars.length;
+console.log("Last car: " + cars[numOfCars - 1]);
+
+cars.push("mercedes-benz");
+console.log(cars [6]);
+
+for (let i = 0; i < cars.length; i++) {
+    console.log(cars[i]);
+
+}
